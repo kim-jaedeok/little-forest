e1ca664fc75599f8e6e58904c82ab7f40960630e
Kim Jaedeok <rn.6d17s2@gmail.com>
2025-04-21 00:12:51 +0900

    refactor: migration to vite

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import reducer from "./reducers";
 
 const middleware = [thunk];
 
-if (process.env.NODE_ENV !== "production") {
+// Vite에서는 import.meta.env를 사용하여 환경 변수에 접근
+if (import.meta.env.MODE !== "production") {
   middleware.push(createLogger());
 }
 
